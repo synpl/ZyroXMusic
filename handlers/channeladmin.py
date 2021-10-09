aaabbb669356336362836d3d0bd1e72e7d9d5054
synpl <92053308+synpl@users.noreply.github.com>
2021-10-09 11:18:07 +0700

    Update channeladmin.py

--- handlers/channeladmin.py
+++ handlers/channeladmin.py
@@ -25,10 +25,10 @@ async def pause(_, message: Message):
     if (chat_id not in callsmusic.pytgcalls.active_calls) or (
         callsmusic.pytgcalls.active_calls[chat_id] == "paused"
     ):
-        await message.reply_text("❌ nothing is playing !")
+        await message.reply_text("❌ Tidak ada lagu yang sedang diputar")
     else:
         callsmusic.pytgcalls.pause_stream(chat_id)
-        await message.reply_text("▶️ music paused!")
+        await message.reply_text("▶️ Istirahat dulu")
 
 
 @Client.on_message(filters.command(["channelresume","cresume"]) & filters.group & ~filters.edited)
@@ -49,7 +49,7 @@ async def resume(_, message: Message):
         await message.reply_text("❌ nothing is paused!")
     else:
         callsmusic.pytgcalls.resume_stream(chat_id)
-        await message.reply_text("⏸ music resumed!")
+        await message.reply_text("⏸ Oke lanjut")
 
 
 @Client.on_message(filters.command(["channelend","cend"]) & filters.group & ~filters.edited)
@@ -65,7 +65,7 @@ async def stop(_, message: Message):
       return    
     chat_id = chid
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❌ nothing in streaming!")
+        await message.reply_text("❌ Tidak ada lagu yg sedang di stream")
     else:
         try:
             callsmusic.queues.clear(chat_id)
@@ -73,7 +73,7 @@ async def stop(_, message: Message):
             pass
 
         callsmusic.pytgcalls.leave_group_call(chat_id)
-        await message.reply_text("⏹ streaming ended!")
+        await message.reply_text("⏹ Istirahat dulu")
 
 
 @Client.on_message(filters.command(["channelskip","cskip"]) & filters.group & ~filters.edited)
@@ -90,7 +90,7 @@ async def skip(_, message: Message):
       return    
     chat_id = chid
     if chat_id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text("❌ nothing to skip!")
+        await message.reply_text("❌ Tidak ada antrian selanjutnya")
     else:
         callsmusic.queues.task_done(chat_id)
 
